03e90aad76fae8eaf302f2e1910d1ecb8e73ef50
Nayef Alotaibi <nayefalotaibi@nayefs-macbook-pro.local>
2018-08-13 04:33:36 -0600

    Finished Scroll ( working on text field )

---
+++ Real Estate/Controllers/AddProteryVC.swift
@@ -0,0 +1,33 @@
+//
+//  AddProteryVC.swift
+//  Real Estate
+//
+//  Created by Nayef Alotaibi on 8/13/18.
+//  Copyright © 2018 Nayef Alotaibi. All rights reserved.
+//
+
+import UIKit
+
+class AddProteryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
+}
---
+++ Real Estate/Controllers/ViewControllerdd.swift
@@ -0,0 +1,21 @@
+//
+//  ViewControllerdd.swift
+//  Real Estate
+//
+//  Created by Nayef Alotaibi on 8/13/18.
+//  Copyright © 2018 Nayef Alotaibi. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerdd: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+
+}
