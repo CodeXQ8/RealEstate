e2b50535433b4dc7ae3805b80e7738f985d5542d
Nayef Alotaibi <nayefalotaibi@nayefs-macbook-pro.local>
2018-08-14 07:17:05 -0600

    Still working on presenting the signVC to access saved and propteryVC

--- Real Estate/Controllers/SavedVC.swift
+++ Real Estate/Controllers/SavedVC.swift
@@ -17,19 +17,57 @@ class SavedVC: UIViewController {
         
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.emptyDataSetSource = self
+        collectionView.emptyDataSetDelegate = self
+        
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         navigationController?.navigationBar.shadowImage = UIImage()
     }
+    
+    deinit {
+        self.collectionView?.emptyDataSetSource = nil
+        self.collectionView?.emptyDataSetDelegate = nil
+    }
+    
 
 }
 
-extension SavedVC: UICollectionViewDelegate, UICollectionViewDataSource {
+extension SavedVC: UICollectionViewDelegate, UICollectionViewDataSource, DZNEmptyDataSetSource, DZNEmptyDataSetDelegate {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return 5
+        return 0
     }
     
+    func title(forEmptyDataSet scrollView: UIScrollView) -> NSAttributedString? {
+        let str = "Welcome to Real Estate"
+        let attrs = [NSAttributedStringKey.font: UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)]
+        return NSAttributedString(string: str, attributes: attrs)
+    }
+    
+    func description(forEmptyDataSet scrollView: UIScrollView) -> NSAttributedString? {
+        let str = "To Access All Feature Of The App Please Sign In"
+        let attrs = [NSAttributedStringKey.font: UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)]
+        return NSAttributedString(string: str, attributes: attrs)
+    }
+    
+    func image(forEmptyDataSet scrollView: UIScrollView) -> UIImage? {
+        return UIImage(named: "realestate")
+    }
+    
+    func buttonTitle(forEmptyDataSet scrollView: UIScrollView, for state: UIControlState) -> NSAttributedString? {
+        let str = "Sign In"
+        let attrs = [NSAttributedStringKey.font: UIFont.preferredFont(forTextStyle: UIFontTextStyle.callout)]
+        return NSAttributedString(string: str, attributes: attrs)
+    }
+    
+    func emptyDataSet(_ scrollView: UIScrollView, didTap button: UIButton) {
+     print("I was clicked")
+       
+        let savedVC = self.storyboard?.instantiateViewController(withIdentifier: "SignUpVC") as! SignUp
+    
+        self.present(savedVC, animated: true, completion: nil)
+    }
     
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
